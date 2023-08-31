dc06663c408f8d140a6842604ed85111bf120a40
platiniromina <platini.romina@gmail.com>
2023-08-31 15:43:46 -0300

    add class Biblioteca

--- src/ar/edu/unlu/poo/tp1/ej6/Biblioteca.java
+++ src/ar/edu/unlu/poo/tp1/ej6/Biblioteca.java
@@ -1,2 +1,40 @@
-package ar.edu.unlu.poo.tp1.ej6;public class Biblioteca {
+package ar.edu.unlu.poo.tp1.ej6;
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Libro> libros = new ArrayList<Libro>();
+
+    public void agregarLibro(String newIsbn, String newTitulo, String newAutor, int newPags, int cantEjemplares) {
+        Libro newLibro = new Libro(newIsbn, newTitulo, newAutor, newPags, cantEjemplares);
+        libros.add(newLibro);
+    }
+    public boolean prestarLibro(String titulo) {
+        Libro libroBuscado = buscar(titulo);
+        if (libroBuscado.prestar()) {
+            return true;
+        }
+        return false;
+    }
+
+    public Libro buscar(String titulo) { // sobrecargar para buscar por titulo, autor
+        for(Libro l: libros) {
+            if (l.getTitulo().equals(titulo)) {
+                return l;
+            }
+        }
+        return null;
+    }
+
+    public int verPrestamos() {
+        int prestados = 0;
+        for(Libro l: libros) {
+            prestados += l.getEjemplaresPrestados();
+        }
+        return prestados;
+    }
+
+    public void mostrarLibro(Libro l) {
+        System.out.println(l.verDescripcion());
+    }
 }
+
