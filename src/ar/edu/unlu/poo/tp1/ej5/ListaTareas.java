537d6103665d014fc2dcb70fff77e6ad53d11a63
platiniromina <platini.romina@gmail.com>
2023-09-01 00:35:20 -0300

    changes ListaTareas

--- src/ar/edu/unlu/poo/tp1/ej5/ListaTareas.java
+++ src/ar/edu/unlu/poo/tp1/ej5/ListaTareas.java
@@ -1,2 +1,32 @@
-package ar.edu.unlu.poo.tp1.ej5;public class ListaTareas {
+package ar.edu.unlu.poo.tp1.ej5;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ListaTareas {
+    private ArrayList<Tarea> listaTareas = new ArrayList<>();
+
+    public void crearListaTareas() {
+        ListaTareas listaTareas = new ListaTareas();
+    }
+    public void crearTarea(String[] descripcion, Prioridad prioridad, Estado estado, LocalDate fechaLimite) {
+        Tarea task = new Tarea(descripcion, prioridad, estado, fechaLimite);
+        listaTareas.add(task);
+    }
+
+    public void mostrarLista() {
+        for(Tarea tarea : listaTareas) {
+            tarea.mostrarTarea();
+        }
+    }
+    public Tarea buscarTarea(String titulo) {
+        for (Tarea tarea : listaTareas) {
+            String[] tituloTarea = tarea.getDescripcion();
+            String tituloBuscado = titulo.toLowerCase();
+            // comparar si cadena 2 contiene cadena 1
+        }
+    }
+    public void tareasOrdenadas() {
+
+    }
 }
